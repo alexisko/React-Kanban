f02d2a6bdd749188e7b345e3b6c5ffff4b5e8468
Alexis <alexisko@hawaii.edu>
2018-03-01 15:39:04 -1000

    Restartin this thang from scratch!

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
 import './index.css';
-import reducers from './reducers';
-import App from './containers/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-let store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(ReduxThunk)
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
