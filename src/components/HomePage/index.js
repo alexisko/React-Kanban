6e245d2f45407aa3c216a71beb45e1091ea774b0
Alexis <alexisko@hawaii.edu>
2018-03-08 17:02:35 -1000

    Started on some styling for homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -4,10 +4,17 @@ import './styles.css';
 
 const HomePage = () => {
   return (
-    <div className="home">
-      <h1>REACT-KANBAN</h1>
-      <Link to="/login">Login</Link>
-      <Link to="/signup">SignUp</Link>
+    <div className="home-page">
+      <div className="home-page__title">
+        <span className="home-page__welcome">Welcome to,</span>
+        <h1>REACT-KANBAN</h1>
+        <span className="home-page__desc">Your online tool for getting sh*t done!</span>
+      </div>
+
+      <div className="home-page__nav">
+        <Link to="/login"><button>Login</button></Link>
+        <Link to="/signup"><button>SignUp</button></Link>
+      </div>
     </div>
   );
 }
